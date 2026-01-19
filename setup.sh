cdaa7aa767448b21f8830a8062d774795eec0c91
Carlos Baraza Haro <carlos.baraza@ieee.org>
2026-01-19 17:56:12 +0100

    Modernize setup.sh: fnm, zsh plugins, symlinks, remove iterm

--- setup.sh
+++ setup.sh
@@ -8,10 +8,9 @@ if [[ "${TRACE-0}" == "1" ]]; then
 fi
 
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo 'Usage: ./script.sh arg-one arg-two
+    echo 'Usage: ./setup.sh
 
-This is an awesome bash script to make your life better.
-
+Sets up dotfiles symlinks and installs common tools.
 '
     exit
 fi
@@ -19,58 +18,77 @@ fi
 cd "$(dirname "$0")"
 
 main() {
+    # Homebrew
+    ################################################################################
+    if ! command -v brew &> /dev/null; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+
     # ZSH
-    ###############################################################################
+    ################################################################################
     # Install oh-my-zsh
-    #sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    if [ ! -d ~/.oh-my-zsh ]; then
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    fi
 
-    # Install theme for shell
-    mkdir -p ~/.oh-my-zsh/custom/themes/
-    curl -L -o ~/.oh-my-zsh/custom/themes/bullet-train.zsh-theme http://raw.github.com/caiogondim/bullet-train-oh-my-zsh-theme/master/bullet-train.zsh-theme
+    # Install zsh plugins
+    ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+    if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+    fi
+    if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+    fi
+
+    # Symlink dotfiles
     rm -f ~/.zshrc; ln -s ~/dotfiles/.zshrc ~/.zshrc
-
-    # Add history if you want to keep the command history
-    # mkdir -p ~/sync
-    # touch ~/.zsh_history
-    # if [ ! -f ~/sync/.zsh_history ]; then # if file not exists
-    #     cp ~/.zsh_history ~/sync/.zsh_history
-    # fi
-    # rm -f ~/.zsh_history
-    # ln -s ~/sync/.zsh_history ~/.zsh_history
-
-    # Install powerline compatible fonts
-    git clone https://github.com/powerline/fonts ~/powerline-fonts
-    ~/powerline-fonts/install.sh
-    rm -rf ~/powerline-fonts
-
+    rm -f ~/.zprofile; ln -s ~/dotfiles/zprofile ~/.zprofile
+    rm -f ~/.tmux.conf; ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
     # GIT
     ################################################################################
     rm -f ~/.gitconfig; ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-    rm -f ~/.gitignore; ln -s ~/dotfiles/git/gitignore ~/.gitignore
 
-
-    # Homebrew
+    # CLI Tools
     ################################################################################
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    brew install node
-    brew install nvm
-    brew install terraform
-    brew install watchman
-
-    # Python
-    brew install pyenv
-
-    # Install GO
+    brew install fnm           # Fast Node Manager (replaces nvm)
+    brew install pyenv         # Python version manager
     brew install go
-    brew install svn
-
-    # Install iterm2
     brew install tmux
-    brew install --cask iterm2
-    brew install --cask warp
+    brew install wget
+    brew install cmake
 
+    # Modern CLI tools
+    brew install dust          # du replacement
+    brew install fd            # find replacement
+    brew install duf           # df replacement
+    brew install eza           # ls replacement (exa is unmaintained)
+    brew install ripgrep       # grep replacement
+    brew install bat           # cat replacement
+    brew install git-delta     # git diff pager
+    brew install sd            # sed replacement
+    brew install pdfgrep       # grep for PDFs
+    brew install fzf           # fuzzy finder
+    brew install zoxide        # smarter cd
+    brew install atuin         # better shell history
+    brew install the_silver_searcher  # ag search
+
+    # Vim
+    brew install neovim
+    if [ ! -d ~/.vim_runtime ]; then
+        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+        sh ~/.vim_runtime/install_awesome_vimrc.sh
+    fi
+
+    # Rust
+    if ! command -v rustc &> /dev/null; then
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    fi
+
+    # Apps (casks)
+    ################################################################################
+    brew install --cask warp
     brew install --cask visual-studio-code
     brew install --cask google-chrome
     brew install --cask chromium --no-quarantine
@@ -78,57 +96,16 @@ main() {
     brew install --cask slack
     brew install --cask istat-menus
     brew install --cask spotify
-    brew install --cask cakebrew
     brew install --cask alfred
     brew install --cask rectangle
     brew install --cask the-unarchiver
     brew install --cask vlc
-    brew install --cask diffmerge
     brew install --cask whatsapp
     brew install --cask docker
-    brew install --cask geekbench
     brew install --cask google-cloud-sdk
     brew install --cask google-drive
 
-    # https://jvns.ca/blog/2022/04/12/a-list-of-new-ish--command-line-tools/
-    brew install dust
-    brew install fd
-    brew install duf
-    brew install exa
-    brew install ripgrep
-    brew install bat
-    brew install git-delta
-    brew install dog
-    brew install glances
-    brew install sd
-    brew install pdfgrep
-    brew install broot
-    brew install choose-rust
-    brew install ctop
-    brew install rga
-    brew install fzf
-    brew install ldns # DNS (drill)
-
-    brew install wget
-    brew install cmake
-
-    # Install macvim
-    brew install macvim
-    ln -s /usr/local/opt/macvim/MacVim.app/Contents/MacOS/Vim /usr/local/bin/vim
-    ln -s /usr/local/opt/macvim/MacVim.app/Contents/MacOS/Vim /usr/local/bin/vi
-    brew install neovim
-
-    # Install vimrc (Cool vim configuration)
-    # https://github.com/amix/vimrc
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-    # Install ag search command
-    brew install the_silver_searcher
-
-    # Rust
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    echo "Done! Restart your terminal or run: source ~/.zshrc"
 }
 
 main "$@"
-
