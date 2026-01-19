9fb2f41030b646420fd74fd94bbd222148b477f9
Carlos Baraza Haro <carlos.baraza@ieee.org>
2026-01-19 17:25:02 +0100

    Remove outdated backup scripts

--- backup-pictures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Backing up 2019 to /Volumes/photo"
-rsync --archive --verbose --human-readable ~/Pictures/2019 /Volumes/photo
-
-echo "Backing up Lightroom to /Volumes/photo"
-rsync --archive --verbose --human-readable ~/Pictures/Lightroom /Volumes/photo
