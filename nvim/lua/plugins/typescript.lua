fdee98befd7225299f7deab63255e6fb1f43d782
Carlos Baraza Haro <carlos.baraza@ieee.org>
2026-01-20 15:07:32 +0100

    tsgo needs update from astronvim side

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -1,13 +1,13 @@
+-- tsgo requires lspconfig v3+ which breaks AstroNvim
+-- Using ts_ls until AstroNvim updates to support new lspconfig API
 return {
   "AstroNvim/astrolsp",
   opts = {
-    servers = {
-      "ts_ls",
-    },
+    servers = { "ts_ls" },
     config = {
       ts_ls = {
         init_options = {
-          maxTsServerMemory = 8192,  -- 8GB, adjust as needed
+          maxTsServerMemory = 8192,
         },
       },
     },
